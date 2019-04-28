bc1de52da2e7485d6c93438a6c62e6ef8059518a
han yu <1807003141@qq.com>
2019-04-28 19:30:07 +0800

    feat: 完成头部 & 脚部结构、样式.

---
+++ mall/src/components/NavFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <section classs="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+          <div class="footer__secondary__nav">
+            <span>Copyright © 2020 小宇商城 All Rights Reserved.</span>
+            <a href="#">About Us</a>
+            <a href="#">Terms &amp; Conditions</a>
+            <a href="#">Privacy Policy</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </footer>
+</template>
---
+++ mall/src/pages/GoodsList/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 顶部导航 -->
+    <nav-header></nav-header>
+    <!-- 面包屑 -->
+    <nav-bread>
+      <span>商品列表</span>
+    </nav-bread>
+    <!-- 尾部说明 -->
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from '@/components/NavHeader';
+import NavBread from '@/components/NavBread';
+import NavFooter from '@/components/NavFooter';
+
+export default {
+  name: 'GoodsList',
+  components: {
+    NavHeader,
+    NavBread,
+    NavFooter
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
